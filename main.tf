9cb5b89e5c4be84f0f6bea62b120338127c24ce1
Aditya Tanwar <86700210+aditya-tanwar@users.noreply.github.com>
2023-02-03 19:48:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,16 +8,3 @@ terraform {
 }
 
 resource "null_resource" "test" {}
-[root@localhost test]# ls
-main.tf
-[root@localhost test]# cat main.tf 
-terraform {
-  required_providers {
-    null = {
-      source = "hashicorp/null"
-      version = "3.2.1"
-    }
-  }
-}
-
-resource "null_resource" "test" {}
