909101837acddbe222fa259a7aeeb1e64482722c
Aditya Tanwar <86700210+aditya-tanwar@users.noreply.github.com>
2023-02-03 19:45:17 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
+}
+
+resource "null_resource" "test" {}
+[root@localhost test]# ls
+main.tf
+[root@localhost test]# cat main.tf 
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
+}
+
+resource "null_resource" "test" {}
